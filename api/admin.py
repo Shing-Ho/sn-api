e2a561982280ce85b038accfb0987e45fa302819
Robert Wheeler <robert.wheeler@truoptik.com>
2020-06-25 11:47:33 -0400

    changed renamed class in admin and serializers

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from api.models.models import mappingcodes
+from api.models.models import hotelmappingcodes
 
 
 class HotelbedsAdmin(admin.ModelAdmin):
     list_per_page = 100
 
 
-admin.site.register(mappingcodes, HotelbedsAdmin)
+admin.site.register(hotelmappingcodes, HotelbedsAdmin)
