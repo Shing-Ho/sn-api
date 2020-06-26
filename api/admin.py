368329b0e14c1120ab8e18c318ef45cb37818f10
anthony <anthony@simplenight.com>
2020-06-26 00:31:28 +0000

    adding additional admin fields

--- api/admin.py
+++ api/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from api.models.models import hotelmappingcodes, sn_hotel_map
-=======
-from api.models.models import hotelmappingcodes
->>>>>>> d980c5612b59558f4cd02be12d62991a9ad024ef
 
 
 class HotelbedsAdmin(admin.ModelAdmin):
     list_per_page = 100
 
 
-<<<<<<< HEAD
 class sn_hotel_mapAdmin(admin.ModelAdmin):
     list_per_page = 100
     list_display = ('simplenight_id', 'provider', 'provider_id',)
@@ -18,6 +13,3 @@ class sn_hotel_mapAdmin(admin.ModelAdmin):
 
 admin.site.register(hotelmappingcodes, HotelbedsAdmin)
 admin.site.register(sn_hotel_map, sn_hotel_mapAdmin)
-=======
-admin.site.register(hotelmappingcodes, HotelbedsAdmin)
->>>>>>> d980c5612b59558f4cd02be12d62991a9ad024ef
