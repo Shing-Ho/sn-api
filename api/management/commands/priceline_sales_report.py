edf750f3fd9c49e2c1eed75b84455df357b07b43
bbshark99 <blue.shark99@outlook.com>
2020-10-15 20:21:59 -0400

    Update email sending

--- api/management/commands/priceline_sales_report.py
+++ api/management/commands/priceline_sales_report.py
@@ -1,10 +1,18 @@
+import requests
+
 from typing import Dict
+from datetime import datetime
 
 from django.core.management import BaseCommand
 
 from api import logger
 
 from api.hotel.adapters.priceline.priceline_sales_report import PricelineSalesReport
+
+MAIL_GUN_URL = "https://api.mailgun.net/v3/sandbox4d76d5beb24f4ba790d3ccf7cda332c4.mailgun.org/messages"
+MAIL_GUN_API_KEY = "4fc764e45639a2008a075f69a0706591-2fbe671d-1bc16189"
+MAIL_GUN_FROM = "Mailgun Sandbox <postmaster@sandbox4d76d5beb24f4ba790d3ccf7cda332c4.mailgun.org>"
+MAIL_GAN_TO = "rob@simplenight.com"
 
 class UnmatchedSaleItem:
     def __init__(self, sale_item: Dict):
@@ -32,7 +40,23 @@ class Command(BaseCommand):
             email_body += str(item) + "\n"
 
         if email_body != "":
-            logger.info(email_body)
+            response = self.send_email(email_body)
+            logger.info(response.json())
         else:
             logger.warn("No differences")
 
+    def send_email(self, text):
+        today = datetime.now().date()
+
+        subject = "PCLN Ghost Report " + str(today)
+
+        return requests.post(
+            MAIL_GUN_URL,
+            auth=("api", MAIL_GUN_API_KEY),
+            data={
+                "from": MAIL_GUN_FROM,
+                "to": MAIL_GAN_TO,
+                "subject": subject,
+                "text": "CSV file has been attached."
+            }
+        )
