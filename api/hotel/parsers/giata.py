5fdc3a7d404237e8524cc973daed70e1a0d47829
jmorton <jmorton@uplift.com>
2020-11-19 07:49:45 -0800

    * Support HotelBeds in Giata Parser

--- api/hotel/parsers/giata.py
+++ api/hotel/parsers/giata.py
@@ -30,9 +30,18 @@ class GiataParser:
         return {
             "priceline_partner_network": Provider.objects.get_or_create(name="priceline")[0],
             "iceportal": Provider.objects.get_or_create(name="iceportal")[0],
+            "hotelbeds": Provider.objects.get_or_create(name="hotelbeds")[0],
         }
 
     def execute(self):
+        existing_giata_hotels = ProviderHotel.objects.filter(provider=self.provider)
+        logger.info(f"Removing {len(existing_giata_hotels)} existing Giata hotels")
+        existing_giata_hotels.delete()
+
+        existing_giata_mappings = ProviderMapping.objects.all()
+        logger.info(f"Removing {len(existing_giata_mappings)} existing Giata mappings")
+        existing_giata_mappings.delete()
+
         pagination_link = ""
         while pagination_link is not None:
             properties_xmlstr = self.get_properties(pagination_link=pagination_link)
@@ -46,6 +55,8 @@ class GiataParser:
 
         for language_code in settings.GEONAMES_SUPPORTED_LANGUAGES:
             self.execute_hotel_guide_for_language(language_code)
+            hotels_for_lang = ProviderHotel.objects.filter(language_code=language_code)
+            logger.info(f"Loaded {len(hotels_for_lang)} hotels for language {language_code}")
 
     def execute_hotel_guide_for_language(self, language_code):
         properties, pagination_link = self.get_hotel_guide_properties(language=language_code)
