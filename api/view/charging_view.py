c179b1d157256a4d60c2b4a64b90847066b791ca
webdatavisualdev <cli698638@gmail.com>
2021-01-08 04:08:01 -0800

    integrate ocm comment and address api
    Signed-off-by: webdatavisualdev <cli698638@gmail.com>

--- api/view/charging_view.py
+++ api/view/charging_view.py
@@ -6,13 +6,13 @@ import json
 
 from api.charging.charging_service import ChargingService
 
+charging_service = ChargingService()
 
 class ChargingViewSet(viewsets.ViewSet):
     @action(detail=False, url_path="poi", methods=["GET"], name="Search charging location")
     def get_poi(self, request: Request):
-        charging = ChargingService()
-        poi = charging.get_poi(request)
-        filteredResponse = list(map((lambda x: {
+        response = charging_service.get_poi(request)
+        filtered_response = list(map((lambda x: {
             "ID": x["ID"],
             "UUID": x["UUID"],
             "UserComments": x["UserComments"],
@@ -24,17 +24,25 @@ class ChargingViewSet(viewsets.ViewSet):
             "NumberOfPoints": x["NumberOfPoints"],
             "GeneralComments": x["GeneralComments"],
             "Connections": x["Connections"],
-        }), poi.json()))
-        return HttpResponse(json.dumps(filteredResponse), content_type="application/json")
+        }), response.json()))
+        return HttpResponse(json.dumps(filtered_response), content_type="application/json")
 
-    @action(detail=False, url_path="referencedata", methods=["GET"], name="Get reference data")
+    @action(detail=False, url_path="reference_data", methods=["GET"], name="Get reference data")
     def get_reference(self, request: Request):
-        charging = ChargingService()
-        reference = charging.get_reference(request)
-        return HttpResponse(reference, content_type="application/json")
+        response = charging_service.get_reference(request)
+        return HttpResponse(response, content_type="application/json")
+
+    @action(detail=False, url_path="address", methods=["GET"], name="Get address")
+    def get_address(self, request: Request):
+        if not(request.GET.get("latitude") and request.GET.get("longitude")):
+            error_message = {
+                "message": "Latitude and longitude are required"
+            }
+            return HttpResponse(json.dumps(error_message), content_type="application/json", status=400)    
+        response = charging_service.get_address(request)
+        return HttpResponse(response, content_type="application/json")
 
     @action(detail=False, url_path="comment", methods=["POST"], name="Post comment")
     def post_comment(self, request: Request):
-        charging = ChargingService()
-        response = charging.post_comment(request)
+        response = charging_service.post_comment(request)
         return HttpResponse(response, content_type="application/json")
