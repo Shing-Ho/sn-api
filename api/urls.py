c20ff2492851f687f8c064cae9bef92a3f6fa4e5
imfsdev <mingsong.webdev@gmail.com>
2021-01-14 22:07:54 +0800

    create carey api
    Signed-off-by: imfsdev <mingsong.webdev@gmail.com>

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ import api.view.search_views
 import api.view.locations
 import api.view.default_view
 import api.view.charging_view
+import api.view.carey_view
 
 router = routers.SimpleRouter(trailing_slash=False)
 
@@ -15,6 +16,7 @@ router.register(r"locations", api.view.locations.LocationsViewSet, basename="loc
 router.register(r"hotels", api.view.hotels_view.HotelViewSet, basename="hotels")
 router.register(r"search", api.view.search_views.AllProductsViewSet, basename="search")
 router.register(r"charging", api.view.charging_view.ChargingViewSet, basename="charging")
+router.register(r"carey", api.view.carey_view.CareyViewSet, basename="carey")
 router.register(r"authentication", api.view.default_view.AuthenticationView, basename="authentication")
 
 urlpatterns = [
