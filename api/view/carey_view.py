aa381627d878710ed159c23cf55b6c96e094c7c3
imfsdev <mingsong.webdev@gmail.com>
2021-01-18 19:42:30 +0800

    update soap request format
    Signed-off-by: imfsdev <mingsong.webdev@gmail.com>

--- api/view/carey_view.py
+++ api/view/carey_view.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.request import Request
 from django.http import HttpResponse
+import json
 
 from api.carey.carey_service import CareyService
 
@@ -10,26 +11,31 @@ carey_service = CareyService()
 
 class CareyViewSet(viewsets.ViewSet):
     @action(detail=False, url_path="rate-inqury", methods=["POST"], name="Get quotes for a journey")
-    def request_quote_inquiry(self, request: Request):
-        response = carey_service.request_quote_inquiry(request)
+    def get_rate_inquiry(self, request: Request):
+        _response = carey_service.get_quote_inquiry(request)
+        response = json.loads(json.dumps(_response, default=str))
         return HttpResponse(response, content_type="application/json")
 
-    @action(detail=False, url_path="book-reservation", methods=["POST"], name="Get reference data")
-    def request_book_reservation(self, request: Request):
-        response = carey_service.request_book_reservation(request)
+    @action(detail=False, url_path="book-reservation", methods=["POST"], name="Book a reservation")
+    def get_add_reservation(self, request: Request):
+        _response = carey_service.get_book_reservation(request)
+        response = json.loads(json.dumps(_response, default=str))
         return HttpResponse(response, content_type="application/json")
 
-    @action(detail=False, url_path="find-reservation", methods=["POST"], name="Get reference data")
-    def find_reservation(self, request: Request):
-        response = carey_service.find_reservation(request)
+    @action(detail=False, url_path="modify-reservation", methods=["POST"], name="Modify a reservation")
+    def get_modify_reservation(self, request: Request):
+        _response = carey_service.get_modify_reservation(request)
+        response = json.loads(json.dumps(_response, default=str))
         return HttpResponse(response, content_type="application/json")
 
-    @action(detail=False, url_path="cancel-reservation", methods=["POST"], name="Get reference data")
-    def cancel_reservation(self, request: Request):
-        response = carey_service.cancel_reservation(request)
+    @action(detail=False, url_path="find-reservation", methods=["POST"], name="Find a reservation")
+    def get_find_reservation(self, request: Request):
+        _response = carey_service.get_find_reservation(request)
+        response = json.loads(json.dumps(_response, default=str))
         return HttpResponse(response, content_type="application/json")
 
-    @action(detail=False, url_path="modify-reservation", methods=["POST"], name="Get reference data")
-    def modify_reservation(self, request: Request):
-        response = carey_service.modify_reservation(request)
+    @action(detail=False, url_path="cancel-reservation", methods=["POST"], name="Cancel a reservation")
+    def get_cancel_reservation(self, request: Request):
+        _response = carey_service.get_cancel_reservation(request)
+        response = json.loads(json.dumps(_response, default=str))
         return HttpResponse(response, content_type="application/json")
