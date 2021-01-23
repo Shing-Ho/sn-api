52cf7fd09d974ec3490117f0bf4062af8c524ddf
webdevboy <aron@simplenight.com>
2021-01-24 00:40:59 +0500

    updated the requirements

--- api/settings.py
+++ api/settings.py
@@ -1,3 +1,4 @@
+######
 """
 Django settings for API project.
 
@@ -35,6 +36,7 @@ ALLOWED_HOSTS = ["*", "simplenight-api-278418.ue.r.appspot.com", "127.0.0.1", "l
 INSTALLED_APPS = [
     "admin_interface",
     "colorfield",
+    "django.contrib.sites",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -46,12 +48,18 @@ INSTALLED_APPS = [
     "api.auth",
     # Third-party
     "rest_framework",
+    "rest_framework.authtoken",
+    "rest_auth",
+    "allauth",
+    "allauth.account",
+    "rest_auth.registration",
     "rest_framework_api_key",
     "corsheaders",
     "sequences.apps.SequencesConfig",
     "knox",
 ]
 
+SITE_ID = 1
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -165,9 +173,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-
 
 
 GEONAMES_CITIES_URL = "https://download.geonames.org/export/dump/cities15000.zip"
@@ -224,3 +231,11 @@ CACHES = {
 }
 
 CACHE_TIMEOUT = 900
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework.authentication.TokenAuthentication",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
