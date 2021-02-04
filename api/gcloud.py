f9b0b124c113c5c7414a3d95202f32a97d05b328
webdevboy <aron@simplenight.com>
2021-02-05 02:36:31 +0500

    google cloud storage added

---
+++ api/gcloud.py
@@ -0,0 +1,21 @@
+"""
+  GoogleCloudStorage extension classes for MEDIA and STATIC uploads
+  """
+from django.conf import settings
+from storages.backends.gcloud import GoogleCloudStorage
+from storages.utils import setting
+from urllib.parse import urljoin
+
+
+class GoogleCloudMediaFileStorage(GoogleCloudStorage):
+    bucket_name = setting("GS_MEDIA_BUCKET_NAME")
+
+    def url(self, name):
+        return urljoin(settings.MEDIA_URL, name)
+
+
+class GoogleCloudStaticFileStorage(GoogleCloudStorage):
+    bucket_name = setting("GS_STATIC_BUCKET_NAME")
+
+    def url(self, name):
+        return urljoin(settings.STATIC_URL, name)
